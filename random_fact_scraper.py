b25efdd21f5d4ba97d7fed1104f1fe9a862ec903
Marcelo Conceição <marcelombc@gmail.com>
2016-12-05 11:52:31 +0100

    [upd] Scraper.

--- random_fact_scraper.py
+++ random_fact_scraper.py
@@ -1,18 +1,23 @@
 #! python3
 """random_fact_scraper.py - Scrape the http://randomfactgenerator.net website."""
 
-from lxml import html
+import os
 import requests
+from flask import Flask
+from lxml import html
 
+app = Flask(__name__)
 
+@app.route("/")
 def main():
-    page = requests.get('http://randomfactgenerator.net')
+    page = requests.get("http://randomfactgenerator.net")
     tree = html.fromstring(page.content)
-    facts = tree.xpath('//div[@id="z"]/text()')
+    facts = tree.xpath("//div[@id='z']/text()")
 
-    print(list(filter(lambda x: x!= '\n\n', facts)))
+    return list(filter(lambda x: x!= "\n\n", facts))
 
 
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
